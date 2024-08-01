fa2baca483fa4ecfeba8d88ededf03fa6311c10b
Arbri Kumria <arbrikumria@Arbris-MacBook-Pro.local>
2024-07-31 22:32:19 -0400

    favicon changed

--- fowbes/src/components/navbar/aboutText.js
+++ fowbes/src/components/navbar/aboutText.js
@@ -22,7 +22,7 @@ const AboutText = () => {
                         Just like the everyday investor has Forbes, memecoins on Solana have <span className="bold">Fowbes</span>.
                     </p>
                     </div>
-                    <p className="text-about" id="body">Looking for your next 10x, but don't know where to start? We got you. Stay tuned for our digital memecoin newsletter <span className="small-logo-sentence">(Coming Phase 4)</span> that gets distributed <span className="bold">weekly</span> to $FOWBES holders. Holders who launch memecoins will also have the chance each month to be featured in one of our issues.</p>
+                    <p className="text-about" id="body">Looking for your next 10x, but don't know where to start? We got you. Stay tuned for our digital memecoin newsletter <span className="small-logo-sentence">(Coming Phase 3)</span> that gets distributed <span className="bold">weekly</span> to $FOWBES holders. Holders who launch memecoins will also have the chance each month to be featured in one of our issues.</p>
                 </div>
             </div>   
     );
